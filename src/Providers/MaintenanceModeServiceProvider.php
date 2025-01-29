1e9905bb4b3168966ae8924156b653f3697af7ec
tsrgtm <gautamtusar2@gmail.com>
2025-01-29 22:56:09 +0545

    change in service provider

--- src/Providers/MaintenanceModeServiceProvider.php
+++ src/Providers/MaintenanceModeServiceProvider.php
@@ -10,29 +10,40 @@ class MaintenanceModeServiceProvider extends ServiceProvider
 {
     public function register()
     {
+        // Merge the package configuration with the application's config
         $this->mergeConfigFrom(__DIR__.'/../../config/maintenance.php', 'maintenance');
+
+        // Register the Artisan command
         $this->commands([
             ToggleMaintenanceCommand::class,
         ]);
+
+        // Automatically add environment variable if it doesn't exist
         $this->addEnvVariable('MAINTENANCE_MODE', 'false');
     }
 
     public function boot()
     {
-        // Load helper file
+        // Load helper file if it exists
         if (file_exists($file = __DIR__.'/Helpers/maintenance.php')) {
             require $file;
         }
 
+        // Publish configuration file with a unique name
         $this->publishes([
-            __DIR__.'/../../config/maintenance.php' => config_path('maintenance.php'),
-        ], 'config');
+            __DIR__.'/../../config/maintenance.php' => config_path('maintenance_mode.php'),
+        ], 'maintenance-config');
 
+        // Publish view files with a unique name
         $this->publishes([
-            __DIR__.'/../../views' => resource_path('views/vendor/maintenance'),
-        ], 'views');
+            __DIR__.'/../../views' => resource_path('views/vendor/maintenance_mode'),
+        ], 'maintenance-views');
 
+        // Load views from the package
         $this->loadViewsFrom(__DIR__.'/../../views', 'maintenance');
+
+        // Register middleware
+        $this->app['router']->aliasMiddleware('maintenance', MaintenanceModeMiddleware::class);
     }
 
     /**
